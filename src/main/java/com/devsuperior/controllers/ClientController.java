bf8e6823788999e09472ec25b2bc03a8e7276a80
Ivanfjr1 <ivanfjr1@gmail.com>
2024-11-16 09:12:40 -0300

    Adicionado ModelMapper, e melhorias

--- src/main/java/com/devsuperior/controllers/ClientController.java
+++ src/main/java/com/devsuperior/controllers/ClientController.java
@@ -1,10 +1,13 @@
 package com.devsuperior.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 import com.devsuperior.Dto.ClientDto;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -18,6 +21,8 @@ import com.devsuperior.entities.Client;
 import com.devsuperior.services.ClientService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Validated
 @RestController
@@ -27,38 +32,32 @@ public class ClientController {
 
 	private final ClientService clientService;
 
-	@PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Client> save(@Valid @RequestBody ClientDto clientDto) {
-		Client clientSaved = clientService.save(clientDto);
-
-		final HttpHeaders httpHeaders = new HttpHeaders();
-		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-		return new ResponseEntity<>(clientSaved, httpHeaders, HttpStatus.OK);
-	}
-
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Client> getFindById(@PathVariable Long id) {
+	public ResponseEntity<ClientDto> findById(@PathVariable Long id) {
 		try {
-			Client client = clientService.findById(id);
-
-			final HttpHeaders httpHeaders = new HttpHeaders();
-			httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-			return new ResponseEntity<>(client, httpHeaders, HttpStatus.OK);
+			ClientDto clientDto = clientService.findById(id);
+			return ResponseEntity.ok(clientDto);
 		} catch (NoSuchElementException e) {
 			throw new ErrorResponseException(HttpStatusCode.valueOf(404), e.getCause());
 		} catch (Exception e) {
 			throw new ErrorResponseException(HttpStatusCode.valueOf(500), e.getCause());
 		}
 	}
-	
+
 	@GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Client>> getAll() {
-		List<Client> clients = clientService.findAll();
-		
-		final HttpHeaders httpHeaders = new HttpHeaders();
-	    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-		return new ResponseEntity<>(clients, httpHeaders, HttpStatus.OK);
+	public ResponseEntity<Page<ClientDto>> findAll(Pageable pageable) {
+		Page<ClientDto> clientDtoPage = clientService.findAll(pageable);
+		return ResponseEntity.ok(clientDtoPage);
 	}
+
+	@PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<ClientDto> insert(@Valid @RequestBody ClientDto clientDto) {
+		ClientDto clientDtoInsert = clientService.insert(clientDto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(clientDtoInsert.getId()).toUri();
+		return ResponseEntity.created(uri).body(clientDtoInsert);
+	}
+
+
 
 	@DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Client>> deleteById(@PathVariable Long id) {
--- src/main/java/com/devsuperior/services/ClientService.java
+++ src/main/java/com/devsuperior/services/ClientService.java
@@ -1,34 +1,44 @@
 package com.devsuperior.services;
 
-import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import com.devsuperior.Dto.ClientDto;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.entities.Client;
 import com.devsuperior.repositories.ClientRepository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ClientService {
 	
 	private final ClientRepository clientRepository;
-	
-	public Client save(ClientDto clientDto) {
-		ModelMapper modelMapper = new ModelMapper();
+	private final ModelMapper modelMapper;
+
+	@Transactional(readOnly = true)
+	public ClientDto findById(Long id) {
+		Optional<Client> clientOptional = clientRepository.findById(id);
+		Client client = clientOptional.orElseThrow(NoSuchElementException::new);
+        return modelMapper.map(client, ClientDto.class);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<ClientDto> findAll(Pageable pageable) {
+		Page<Client> clients = clientRepository.findAll(pageable);
+        return clients.map(client -> modelMapper.map(client, ClientDto.class));
+	}
+
+	public ClientDto insert(ClientDto clientDto) {
 		Client client = modelMapper.map(clientDto, Client.class);
-		return clientRepository.save(client);
-	}
-	
-	public Client findById(Long id) {
-		return clientRepository.findById(id).orElseThrow();
-	}
-	
-	public List<Client> findAll() {
-		return clientRepository.findAll();
+		Client clientSaved = clientRepository.save(client);
+		return modelMapper.map(clientSaved, ClientDto.class);
 	}
 
 	public void deleteById(Long id) {
